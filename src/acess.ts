7b5e0639daa83f58dd84f3e9cda840282694c227
Xander Azuaje <charon-mortis@pm.me>
2024-08-08 11:23:30 +0200

    added types

--- src/acess.ts
+++ src/acess.ts
@@ -1,4 +1,5 @@
 import {Access, FieldAccess} from "payload/types";
+import {User} from "@/payload-types";
 
 export const isAdmin: Access<any, User> = ({req: {user}}) => {
     return Boolean(user?.roles?.includes('admin'))
