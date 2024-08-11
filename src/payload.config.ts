f162b5a62759d715804b25ed53bab14b3d0cab6b
Xander Azuaje <charon-mortis@pm.me>
2024-08-11 22:50:38 +0200

    added s3 bucket integration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,11 +5,24 @@ import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import { buildConfig } from 'payload/config'
+import { cloudStorage } from "@payloadcms/plugin-cloud-storage";
+import {s3Adapter} from "@payloadcms/plugin-cloud-storage/s3";
 
 import Users from './collections/Users'
 import Posts from "./collections/Posts";
 import Media from "./collections/Media";
 import Categories from "./collections/Categories";
+
+const adapter = s3Adapter({
+  config: {
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY_ID,
+      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    },
+    region: process.env.S3_REGION,
+  },
+  bucket: process.env.S3_BUCKET
+})
 
 export default buildConfig({
   admin: {
@@ -24,7 +37,16 @@ export default buildConfig({
   graphQL: {
     schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
-  plugins: [payloadCloud()],
+  plugins: [
+      payloadCloud(),
+      cloudStorage({
+        collections: {
+          'media': {
+            adapter: adapter
+          }
+        }
+      })
+  ],
   db: mongooseAdapter({
     url: process.env.DATABASE_URI,
   }),
